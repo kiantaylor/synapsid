5fea736b00a3e45a9ef0ee6dfbaf30e33496f802
Kian Taylor <kianbtaylor@gmail.com>
2023-09-16 16:15:11 +0100

    added whole skeleton

---
+++ epq/assets/viewports/morganucodonViewport/newboneback.gd
@@ -0,0 +1,54 @@
+extends Marker3D
+var rootnode=null
+var hover=false
+var readyup=false
+var selected=false
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+		if Input.is_action_just_pressed("leftclick") and hover:
+			print('click')
+		
+			selected=true
+			get_node("MeshInstance3D2").mesh.surface_set_material(0,load("res://glow.tres"))
+		elif Input.is_action_just_pressed("leftclick"):
+			selected=false
+			get_node("MeshInstance3D2").mesh.surface_set_material(0,load("res://bone.tres"))
+		if selected:
+			if Input.is_action_pressed("bonedown"):
+				rotate_x(0.05)
+			elif Input.is_action_pressed("boneup"):
+				rotate_x(-0.05)
+		
+
+
+
+
+func _on_morganucodon_viewport_declare(node):
+	rootnode=node
+	print('yh')
+	readyup=true
+
+
+func _on_area_3d_mouse_entered():
+	hover =true
+	print('hover',name,hover)
+
+
+
+func _on_area_3d_mouse_exited():
+	hover =false
+	print('hover',name)
+
+
+func _on_area_3d_2_mouse_entered():
+	pass # Replace with function body.
+
+
+func _on_area_3d_2_mouse_exited():
+	pass # Replace with function body.
--- epq/assets/viewports/viewport.gd
+++ epq/assets/viewports/viewport.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-var pivotRotationy=0.0
+var pivotRotationy=PI/2.0
 var pivotRotationx=0.0
 var campos=1.9
 var camRotationPos=Vector3()
@@ -8,54 +8,26 @@ var mouse=Vector2()
 var rotateMouse=Vector3(0,0,0)
 var pointerRotation=Vector3(0,0,0)
 var selectedbone=null
+signal declare(node)
 signal clearHover(ignore)
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
+	emit_signal("declare",self)
 	get_node("AnimationPlayer").play('startup')
 
-func _input(event):
-	if event is InputEventMouseMotion:
-		mouse=event.position
-		mouse.y-=648
-		mouse.y/=-1906
-		mouse.x-=1152
-		mouse.x/=3491
 		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	get_node('anchor').rotate_x(0.01)
-	#3d cursor
-	camRotationPos=get_node("pivot/Camera3D").position.rotated(Vector3(1,0,0),get_node('pivot').rotation.x)
-	camRotationPos=camRotationPos.rotated(Vector3(0,1,0),get_node('pivot').rotation.y)
-	camRotationPos=camRotationPos.rotated(Vector3(0,0,1),get_node('pivot').rotation.z)
-	rotateMouse.x=mouse.x
-	rotateMouse.y=mouse.y
-	#print(rotateMouse,'beforerotate')
-	rotateMouse=rotateMouse.rotated(Vector3(1,0,0),get_node('pivot').rotation.x)
-	rotateMouse=rotateMouse.rotated(Vector3(0,1,0),get_node('pivot').rotation.y)
-	rotateMouse=rotateMouse.rotated(Vector3(0,0,1),get_node('pivot').rotation.z)
-	#print(rotateMouse,'after rotate')
-	get_node("3dcursor").position=camRotationPos+rotateMouse
-	pointerRotation.y=lerp(0.925,-0.925,(mouse.x+0.33)/0.66)
-	pointerRotation.x=lerp(-0.401,0.401,(mouse.y+0.34)/0.68)
-	get_node('3dcursor').rotation=get_node("pivot").rotation+pointerRotation
-	if get_node("3dcursor/RayCast3D").is_colliding():
-		get_node('point').position=get_node("3dcursor/RayCast3D").get_collision_point()
-#	var camPoint=Vector3()
-#	camPointcamPoint=get_node('point').position.rotated(Vector3(1,0,0),get_node('pivot').rotation.x)
-#	camPoint=camPoint.rotated(Vector3(0,1,0),get_node('pivot').rotation.y)
-#	camPoint=camPoint.rotated(Vector3(0,0,1),get_node('pivot').rotation.z)
-#	get_node("pivot/Area3D").position=
-	get_node("Area3D").rotation=get_node('pivot').rotation
-	if not Input.is_action_pressed("leftclick"):
-		get_node("Area3D").position=get_node('point').position
-		get_node("Area3D/CollisionShape3D").disabled=true
-	else:
-		
-		get_node("Area3D/CollisionShape3D").disabled=false
 	
-	
+	if selectedbone!=null:
+		if selectedbone.name[0]=='v':
+			
+			if Input.is_action_pressed("bonedown"):
+				selectedbone.rotate_x(-0.025)
+			elif Input.is_action_pressed("boneup"):
+				selectedbone.rotate_x(0.025)
 	# other stuff 
 	get_node('pivot').rotation.y=lerp(get_node('pivot').rotation.y,pivotRotationy,delta)
 	get_node('pivot').rotation.x=lerp(get_node('pivot').rotation.x,pivotRotationx,delta)
@@ -79,11 +51,3 @@ func _process(delta):
 		campos-=0.3
 
 
-func bonehovered(node):
-	emit_signal('clearHover',node)
-
-
-
-
-func boneselected(node):
-	selectedbone=node
